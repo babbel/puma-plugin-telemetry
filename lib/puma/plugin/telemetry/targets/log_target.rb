8a71d55738df14e443dccf6f6f45ad67271cbbfb
Steven Harman <steven@harmanly.com>
2024-05-02 11:33:07 -0400

    Add new LogTarget
    Which defaults to the `:json` formatter with a `:passthrough` transform.
    For now…

---
+++ lib/puma/plugin/telemetry/targets/log_target.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+require 'logger'
+require_relative 'base_formatting_target'
+
+module Puma
+  class Plugin
+    module Telemetry
+      module Targets
+        # Simple Log Target, publishing metrics to a Ruby ::Logger at stdout
+        # at the INFO log level
+        class LogTarget < BaseFormattingTarget
+          def initialize(logger: ::Logger.new($stdout), formatter: :json, transform: :passthrough)
+            super(formatter: formatter, transform: transform)
+            @logger = logger
+          end
+
+          def call(telemetry)
+            logger.info(formatter.call(transform.call(telemetry)))
+          end
+
+          private
+
+          attr_reader :logger
+        end
+      end
+    end
+  end
+end
