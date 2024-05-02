2ff2e4a069231392c13ac1148ebc799b5964f20c
Steven Harman <steven@harmanly.com>
2024-05-02 14:53:27 -0400

    Extract "transform" concept
    As it was, the formatters were doing both transforms on the keys
    (specific to a particular service) AND formatting the telemetry data.
    By separating those responsibilities, we can mix and match different
    Targets, Formatters, and Transforms to get the output needed for
    different consumers.

---
+++ lib/puma/plugin/telemetry/formatters/json_formatter.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+require 'json'
+
+module Puma
+  class Plugin
+    module Telemetry
+      module Formatters
+        # JSON formatter for IO, expects `call` method accepting telemetry hash
+        class JSONFormatter
+          def self.call(telemetry)
+            ::JSON.dump(telemetry)
+          end
+        end
+      end
+    end
+  end
+end
