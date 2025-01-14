1b729fcba7f90082b677619be8e9625b6445007d
Josh Westbrook <josh.westbrook@storable.com>
2025-01-14 09:16:04 -0600

    fix: Don't flush metrics when they're recorded by default

--- lib/puma/plugin/telemetry/targets/open_telemetry_target.rb
+++ lib/puma/plugin/telemetry/targets/open_telemetry_target.rb
@@ -13,11 +13,12 @@ module Puma
         #     OpenTelemetryTarget.new(meter_provider: OpenTelemetry.meter_provider, prefix: 'puma')
         #
         class OpenTelemetryTarget
-          def initialize(meter_provider:, prefix: nil, suffix: nil, attributes: {})
+          def initialize(meter_provider:, prefix: nil, suffix: nil, force_flush: false, attributes: {})
             @meter_provider = meter_provider
             @meter          = meter_provider.meter('puma.telemetry')
             @prefix         = prefix
             @suffix         = suffix
+            @force_flush    = force_flush
             @attributes     = attributes
             @instruments    = {}
           end
@@ -29,15 +30,17 @@ module Puma
           # before they get flushed from the aggregation thread, and when they
           # do, only the last values will get sent.
           #
-          # That's why we are explicitly calling force_flush here, in order to persist
+          # That's why we provide the option to explicitly call force_flush here, in order to persist
           # all metrics, and not only the most recent ones.
+          #
+          # Note: Force flushing metrics every time can significantly impact performance
           #
           def call(telemetry)
             telemetry.each do |metric, value|
               instrument(metric).record(value, attributes: @attributes)
             end
 
-            @meter_provider.force_flush
+            @meter_provider.force_flush if @force_flush
           end
 
           def instrument(metric)
